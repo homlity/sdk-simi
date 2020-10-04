429e861bf76c399512c466ad2a86ed3ea87fa35d
FuriosoJack <iam@furiosojack.com>
2020-10-04 13:56:46 -0500

    Se hace correcion de palabra de metodo

---
+++ src/Application/Models/PaginatorHTML.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Codwelt\SIMI\SDK\Application\Models;
+
+/**
+ * Class PaginatorHTML
+ * @package Codwelt\SIMI\SDK\Application\Models
+ * @author Juan Diaz <iam@furiosojack.com>
+ */
+class PaginatorHTML
+{
+
+}
