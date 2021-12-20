6168a6281a557833815fd111921065bb664d7ffa
FuriosoJack <iam@furiosojack.com>
2021-12-20 10:40:40 -0500

    Se añade nueva data

--- src/InfraStructure/Modelos/InmuebleDetail.php
+++ src/InfraStructure/Modelos/InmuebleDetail.php
@@ -259,6 +259,11 @@ class InmuebleDetail extends InmueblePreview
         return $this->jsonRAW["Direccion"];
     }
 
+    public function fotos360()
+    {
+        return $this->jsonRAW["fotos360"];
+    }
+
 
     public function valorVenta(bool $format = true)
     {
